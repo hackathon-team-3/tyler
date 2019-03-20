c05fa3b41a813c399101b0046fbb1b9ace0aeac3
gmlp <gmlp.24a@gmail.com>
2019-03-20 13:00:57 -0400

    ADD lb ip variable

--- main.tf
+++ main.tf
@@ -4,11 +4,16 @@ variable "students" {
  default = ["Tyler","Roger","John","Gonzalo"]
 }
 
+variable "lb_ip" {
+  default = "127.0.0.1"
+}
+
+
 data "template_file" "wetty-values"{
     count = "${length(var.students)}"
     template = "${file("./wetty-values.yaml")}"
     vars {
-        lb_ip = "127.0.0.1"
+        lb_ip = "${var.lb_ip}"
         user = "${lower(element(var.students,count.index))}"
     }
 }
